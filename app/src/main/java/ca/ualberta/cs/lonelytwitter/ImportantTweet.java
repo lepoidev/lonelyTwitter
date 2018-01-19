2e24d72ba5cc6435bfec95a6806123323d703d95
Kyle LePoidevin-Gonzales <lepoidev@cs.ualberta.ca>
2018-01-18 18:12:37 -0700

    lab demo

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
@@ -0,0 +1,23 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+/**
+ * Created by lepoidev on 1/18/18.
+ */
+
+public class ImportantTweet extends Tweet {
+
+    public ImportantTweet(String message){
+        super(message);
+    }
+
+    public ImportantTweet(String message, Date date){
+        super(message, date);
+    }
+
+    @Override
+    public Boolean isImportant() {
+        return Boolean.TRUE;
+    }
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
@@ -0,0 +1,9 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by lepoidev on 1/18/18.
+ */
+
+public class TweetTooLongException extends Exception{
+
+}
